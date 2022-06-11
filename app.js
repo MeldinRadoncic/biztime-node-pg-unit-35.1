7585210777d36e6993ba82418a450d2b89247940
Meldin Radoncic <meldinradoncic89@gmail.com>
2022-06-10 20:39:42 -0400

    Node pg

---
+++ app.js
@@ -0,0 +1,45 @@
+/** BizTime express application. */
+
+
+const express = require("express");
+const ExpressError = require("./expressError")
+const companiesRoutes = require("./routes/companies");
+const invoicesRoutes = require("./routes/invoices");
+
+const app = express();
+
+app.use(express.json());
+
+
+
+app.use('/companies', companiesRoutes)
+app.use('/invoices', invoicesRoutes)
+
+/** 404 handler */
+
+app.use(function(req, res, next) {
+  const err = new ExpressError("Not Found", 404);
+  return next(err);
+});
+
+/** general error handler */
+
+app.use(function (err, req, res, next) {
+  // the default status is 500 Internal Server Error
+  let status = err.status || 500;
+
+  // set the status and alert the user
+  return res.status(status).json({
+    error: {
+      message: err.message,
+      status: status
+    }
+  });
+});
+
+app.listen(5000,()=> {
+  console.log('Server is running on port 5000')
+})
+
+
+module.exports = app;
